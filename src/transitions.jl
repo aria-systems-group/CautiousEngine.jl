119cf91ec6e79c5a6917bd7b8ffddb0ee94c47da
John Jackson <jmcjack@gmail.com>
2020-11-17 12:19:16 -0700

    Fix manual epsilon parameter usage.

--- src/transitions.jl
+++ src/transitions.jl
@@ -13,8 +13,6 @@ function region_pair_transitions(region_pair, region_dict, region_post_dict, ϵ,
     end
 
     # For now, do a naiive shrink - first noise, then the next
-    # TODO: Pass in real noise value
-    
     if !isnothing(params.system_params.process_noise_dist)
         ϵ_noise = params.data_params.eta
         Pr_noise = (cdf(params.system_params.process_noise_dist, ϵ_noise) - cdf(params.system_params.process_noise_dist, -ϵ_noise))^length(σ_bounds)
@@ -41,11 +39,8 @@ function region_pair_transitions(region_pair, region_dict, region_post_dict, ϵ,
     axes = I+zeros(n_dims, n_dims)
 
     if ϵ < 0
-        # ϵ_lower = lower_bound_ϵ(Q_post_extent, Sshrink, abs(S["x1"][1] - S["x1"][2])) 
-        # ϵ_upper = upper_bound_ϵ(Q_post_extent, Sexpand)
         shrink_int, shrink_ϵ = does_shape2_contain_shape1(Q_shape, s_shape, axes=axes)
         expand_int, expand_ϵ = do_shapes_intersect(Q_shape, S_shape, axes=axes)
-        # @assert !(shrink_int && !expand_int)
         if (shrink_int && !expand_int)
             @info "Shrink Flag: ", shrink_int, "Expand Flag: ", expand_int
             @info "Q Post Extent: ", Q_post_extent 
@@ -58,23 +53,28 @@ function region_pair_transitions(region_pair, region_dict, region_post_dict, ϵ,
             @info "Shrink Ep: ", shrink_ϵ, "Expand Ep: ", expand_ϵ 
             @error "You cannot both interesect and not intersect at the same time!"
         end
+
+        if shrink_int 
+            P_bound_lower = calculate_probability_bound(gp_info_dict, σ_bounds, shrink_ϵ)
+            minPr_LB = P_bound_lower*Pr_noise 
+        else
+            minPr_LB = 0.
+        end
+    
+        if !expand_int 
+            Pr_bound_upper = calculate_probability_bound(gp_info_dict, σ_bounds, expand_ϵ)
+            maxPr_UB = 1. - Pr_bound_upper
+        else
+            maxPr_UB = 1.
+        end
     else
         shrink_ϵ = ϵ
         expand_ϵ = ϵ
-    end
 
-    if shrink_int 
         P_bound_lower = calculate_probability_bound(gp_info_dict, σ_bounds, shrink_ϵ)
         minPr_LB = P_bound_lower*Pr_noise 
-    else
-        minPr_LB = 0.
-    end
-
-    if !expand_int 
         Pr_bound_upper = calculate_probability_bound(gp_info_dict, σ_bounds, expand_ϵ)
         maxPr_UB = 1. - Pr_bound_upper
-    else
-        maxPr_UB = 1.
     end
 
     @assert maxPr_UB >= minPr_LB
@@ -94,35 +94,6 @@ function extent_to_shape(extent)
     return Dict("vertices" => vertices[:])
  end
 
-# function lower_bound_ϵ(Q, S, ϵ_max)
-#     # If all of these checks are false, then Q is necessarily inside of S and !(...) is true.
-#     I_S = !(Q["x1"][1] < S["x1"][1] || Q["x1"][2] > S["x1"][2] || Q["x2"][1] < S["x2"][1] || Q["x2"][2] > S["x2"][2]) #and_point_of_Q_post_outside_of_S_small
-#     if I_S
-#         # How much can we shrink S until there is a point in Q that is outside...
-#         ϵ = minimum([abs(Q["x1"][1] - S["x1"][1]), abs(Q["x1"][2] - S["x1"][2]), abs(Q["x2"][1] - S["x2"][1]), abs(Q["x2"][2] - S["x2"][2]), ϵ_max]) 
-#     else
-#         # If Q has a point outside of S, epsilon is arbitrary. 
-#         ϵ = -1.
-#     end
-#     return ϵ
-# end
-
-# function upper_bound_ϵ(Q, S)
-#     # If all of these checks are true, then Q necessarily intersects S and (...) is true.
-#     I_S = (Q["x1"][1] < S["x1"][2] && Q["x1"][2] > S["x1"][1] && Q["x2"][2] > S["x2"][1] && Q["x2"][1] < S["x2"][2])
-#     if I_S
-#         # Epsilon is arbitrary
-#         ϵ = -1.
-#     else
-#         # How much can we expand S until we get an intersection?
-#         y_min = minimum([abs(Q["x2"][1] - S["x2"][1]), abs(Q["x2"][2] - S["x2"][2]), abs(Q["x2"][2] - S["x2"][1]), abs(Q["x2"][1] - S["x2"][2])]) 
-#         x_min = minimum([abs(Q["x1"][1] - S["x1"][1]), abs(Q["x1"][2] - S["x1"][2]), abs(Q["x1"][2] - S["x1"][1]), abs(Q["x1"][1] - S["x1"][2])]) 
-#         ϵ = maximum([y_min, x_min])
-#         @assert ϵ > 0
-#     end
-#     return ϵ
-# end
-
 function calculate_probability_bound(gp_info_dict, σ_bounds, ϵ)
     Pr_bound = 1.
     for (i, dim_key) in enumerate(keys(gp_info_dict)) 
@@ -140,6 +111,5 @@ function calculate_probability_bound(gp_info_dict, σ_bounds, ϵ)
         end
 
     end
-    # @info Pr_bound, ϵ
     return Pr_bound
 end
