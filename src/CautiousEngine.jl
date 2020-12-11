4f75aec02f929baefcfc7b0c71dbb528d68e3233
John Jackson <jmcjack@gmail.com>
2020-12-11 13:25:27 -0700

    Add prototype code for local GPs

--- src/CautiousEngine.jl
+++ src/CautiousEngine.jl
@@ -360,16 +360,19 @@ function initialize_log(params; logging=Logging.Info)
     return logfile
 end
 
-function generate_estimates(params, x_train, y_train; reuse_gp_flag=false)
+function generate_estimates(params, x_train, y_train; reuse_gp_flag=false, filename_appendix=nothing)
     exp_dir = create_experiment_directory(params)
     # Generate a set of GPRs if none are provided.
-    gps_dir = @sprintf("%s/gps", params.experiment_directory)
+    gps_dir = @sprintf("%s/gps", exp_dir)
     !isdir(gps_dir) && mkpath(gps_dir)
-    gps_filename = @sprintf("%s/%s-m%d-σ%1.3f-rs%d-gps.bin", gps_dir, params.system_params.mode_tag, params.data_params.data_num, params.data_params.noise_sigma, params.random_seed)
+    gps_filename = @sprintf("%s-m%d-σ%1.3f-rs%d-gps", params.system_params.mode_tag, params.data_params.data_num, params.data_params.noise_sigma, params.random_seed)
+    gps_filename = isnothing(filename_appendix) ? gps_filename : "$gps_filename-$filename_appendix"
+    local_gp_file = "$exp_dir/$gps_filename.bin"
     data_deps = params.system_params.dependency_dims
-    if isfile(gps_filename) && reuse_gp_flag
-        @info "Resuing gp: " gps_filename
-        open(gps_filename) do f
+
+    if isfile(local_gp_file) && reuse_gp_flag
+        @info "Resuing gp: " local_gp_file 
+        open(local_gp_file) do f
             gp_set = deserialize(f)
         end
     else 
@@ -383,9 +386,9 @@ function generate_estimates(params, x_train, y_train; reuse_gp_flag=false)
             m_prior = MeanZero()
             k_prior = SE(ls, 0.)
             lnoise = log(sqrt(1+2/params.data_params.data_num)) # Generalize to handle any bound
-            opt_idx = StatsBase.sample(1:length(y_train[:,1]), params.data_params.data_num_optimize, replace = false)
-            gp_pre = GP(x_train_sub[opt_idx, :]', y_train[opt_idx,i], m_prior, k_prior, lnoise) 
-            optimize!(gp_pre)
+            # opt_idx = StatsBase.sample(1:length(y_train[:,1]), params.data_params.data_num_optimize, replace = false)
+            # gp_pre = GP(x_train_sub[opt_idx, :]', y_train[opt_idx,i], m_prior, k_prior, lnoise) 
+            # optimize!(gp_pre)
             gp = GP(x_train_sub', y_train[:,i], m_prior, k_prior, lnoise)
             gp_set["x$i"] = deepcopy(gp)
         end
@@ -402,24 +405,31 @@ function generate_estimates(params, x_train, y_train; reuse_gp_flag=false)
     return gp_set, gp_info_dict
 end
 
-function save_gp_info(params, gp_set, gp_info_dict)
+function save_gp_info(params, gp_set, gp_info_dict; save_global_gps=true, filename_appendix=nothing)
     exp_dir = create_experiment_directory(params)
-    gps_dir = @sprintf("%s/gps", params.experiment_directory)
-    !isdir(gps_dir) && mkpath(gps_dir)
-    gps_filename = @sprintf("%s/%s-m%d-σ%1.3f-rs%d-gps.bin", gps_dir, params.system_params.mode_tag, params.data_params.data_num, params.data_params.noise_sigma, params.random_seed)
-    # Save the GPs for further analysis. 
-    @info "Saving GP regressions to experiment directory..."
-    open(gps_filename, "w") do f
-        serialize(f, gp_set)
+    if save_global_gps
+        gps_dir = @sprintf("%s/gps", params.experiment_directory)
+        !isdir(gps_dir) && mkpath(gps_dir)
+        gps_filename = @sprintf("%s/%s-m%d-σ%1.3f-rs%d-gps.bin", gps_dir, params.system_params.mode_tag, params.data_params.data_num, params.data_params.noise_sigma, params.random_seed)
+        # Save the GPs for further analysis. 
+        @info "Saving GP regressions to experiment directory..."
+        open(gps_filename, "w") do f
+            serialize(f, gp_set)
+        end
     end
-    local_gp_file = "$exp_dir/gaussian_processes_set.bin"
+    
+    local_gp_dir = "$exp_dir/gps"
+    !isdir(local_gp_dir) && mkpath(local_gp_dir)
+    gps_filename = @sprintf("%s-m%d-σ%1.3f-rs%d-gps", params.system_params.mode_tag, params.data_params.data_num, params.data_params.noise_sigma, params.random_seed)
+    gps_filename = isnothing(filename_appendix) ? gps_filename : "$gps_filename-$filename_appendix"
+    local_gp_file = "$local_gp_dir/$gps_filename.bin"
     open(local_gp_file, "w") do f
         serialize(f, gp_set)
     end
     # TODO: Save the entire info dict
 end
 
-function generate_region_images(params, gp_info_dict; reuse_regions_flag=false)
+function generate_region_images(params, gp_info_dict::Dict; reuse_regions_flag=false)
 
     exp_dir = create_experiment_directory(params)
     region_filename = "$exp_dir/regions.bson"
@@ -465,12 +475,80 @@ function generate_region_images(params, gp_info_dict; reuse_regions_flag=false)
     return region_data
 end
 
+function get_points_in_neighborhood(center, radius, x_train, y_train)
+    norm_array = [norm(row) for row in eachrow(center .- x_train)]
+    bool_array = norm_array .<= radius
+    x_sub = @view x_train[bool_array, :]
+    y_sub = @view y_train[bool_array, :]
+    return x_sub, y_sub
+end
+
+function generate_region_images(params, x_train, y_train; reuse_regions_flag=false)
+
+    exp_dir = create_experiment_directory(params)
+    region_filename = "$exp_dir/regions.bson"
+
+    if reuse_regions_flag && isfile(region_filename)
+        region_data = BSON.load(region_filename)
+    else
+        region_dict, region_pairs = create_region_data(params.domain, params.discretization_step)
+        num_regions = length(keys(region_dict))
+        region_post_dict = Dict()
+        region_gp_dict = Dict()
+
+        # Minimum and maximum extents
+        domain = params.domain
+        min_ex = [domain[dim_key][1] for dim_key in keys(domain)]
+        max_ex = [domain[dim_key][2] for dim_key in keys(domain)]
+
+        # Store the predicted mean and covariance for each sampled point 
+        dim_keys = keys(domain)
+        for i=1:num_regions
+            # Get subset of data here
+            @info "Bounding region $i/$num_regions"
+            if i == num_regions
+                extent = region_dict[-11]
+                lb = [extent[dim_key][1] for dim_key in dim_keys]
+                ub = [extent[dim_key][2] for dim_key in dim_keys]
+                # Assume 2D
+                gp_set, gp_info_dict = generate_estimates(params, x_train, y_train, filename_appendix=i) 
+                save_gp_info(params, gp_set, gp_info_dict, save_global_gps=false, filename_appendix=i)
+                region_post_dict[i] = bound_extent(extent, lb, ub, gp_info_dict, dim_keys, params.system_params.dependency_dims; known_part_flag=!isnothing(params.system_params.known_dynamics_fcn))
+                region_gp_dict[i] = gp_info_dict 
+            else
+                extent = region_dict[i]
+                lb = [extent[dim_key][1] for dim_key in dim_keys]
+                ub = [extent[dim_key][2] for dim_key in dim_keys]
+                # Assume 2D
+                center = [mean(lb) mean(ub)]
+                radius = 0.5
+                x_sub, y_sub = get_points_in_neighborhood(center, radius, x_train, y_train)
+                gp_set, gp_info_dict = generate_estimates(params, x_sub, y_sub, filename_appendix=i) 
+                save_gp_info(params, gp_set, gp_info_dict, save_global_gps=false, filename_appendix=i)
+                region_post_dict[i] = bound_extent(extent, lb, ub, gp_info_dict, dim_keys, params.system_params.dependency_dims; known_part_flag=!isnothing(params.system_params.known_dynamics_fcn))
+                region_gp_dict[i] = gp_info_dict
+            end
+        end  # End threaded forloop
+
+        region_data = Dict()
+        region_data[:pairs] = region_pairs
+        region_data[:extents] = region_dict
+        region_data[:posts] = region_post_dict
+        region_data[:gps] = region_gp_dict
+    end
+
+    return region_data
+end
+
 function save_region_data(params, region_data)
     @info "Saving the region data to the experiment directory..."
     exp_dir = create_experiment_directory(params)
     region_filename = "$exp_dir/regions.bson"
-
-    bson(region_filename, region_data)
+    region_data_save = Dict()
+    region_data_save[:pairs] = region_data[:pairs]
+    region_data_save[:extents] = region_data[:extents]
+    region_data_save[:posts] = region_data[:posts]
+    bson(region_filename, region_data_save)
 end
 
 "Generates transition bounds with the given parameters."
@@ -515,6 +593,51 @@ function generate_transition_bounds(params, gp_info_dict, region_data;
     res_mats = Dict("minPr" => minPr_mat, "maxPr" => maxPr_mat)
     return res_mats 
 end
+
+"Generates transition bounds with the given parameters."
+function generate_transition_bounds(params, region_data; 
+                                    reuse_mats_flag=false) 
+
+    exp_dir = create_experiment_directory(params)
+    region_pairs = region_data[:pairs]
+    region_dict = region_data[:extents]
+    region_post_dict = region_data[:posts]
+    region_gp_dict = region_data[:gps]
+
+    tmat_filename = "$exp_dir/transition_mats.mat"
+    if !reuse_mats_flag || !isfile(tmat_filename)
+        @info "Calculating transition probability bounds between regions..."
+        results_df = process_foo(region_pairs, region_dict, region_post_dict, region_gp_dict, params.data_params.epsilon, params)
+
+        # Save the matrices for use in MATLAB verification tool
+        num_states = length(keys(region_dict))
+        minPr_mat = zeros((num_states, num_states))
+        maxPr_mat = zeros((num_states, num_states))
+        for i = 1:1:num_states
+            sub_row = results_df[results_df.Set1 .== i, :]
+            if i == num_states
+                minPr_mat[i, i] = 1.
+                maxPr_mat[i, i] = 1.
+            else
+                for j = 1:1:num_states
+                    if j == num_states
+                        subsub_row = sub_row[sub_row.Set2 .== -11, :]
+                        minPr_mat[i, j] = 1. - subsub_row.MaxPr[1]
+                        maxPr_mat[i, j] = 1. - subsub_row.MinPr[1]
+                    else
+                        subsub_row = sub_row[sub_row.Set2 .== j, :]
+                        minPr_mat[i, j] = subsub_row.MinPr[1]
+                        maxPr_mat[i, j] = subsub_row.MaxPr[1]
+                    end
+                end
+            end
+        end
+    end
+
+    res_mats = Dict("minPr" => minPr_mat, "maxPr" => maxPr_mat)
+    return res_mats 
+end
+
 
 function save_transition_matrices(params, res_mats)
     exp_dir = create_experiment_directory(params)
@@ -608,6 +731,56 @@ function end_to_end_transition_bounds(params; single_mode_verification=false, re
     return timing_info, safety_result_mat, result_mats
 end
 
+function end_to_end_transition_bounds_local_gps(params; single_mode_verification=false, reuse_regions_flag=false) 
+    logfile = initialize_log(params)
+    @info "Generating the training data..."
+    total_runtime = 0.
+    timing_info = Dict()
+    gen_time = @elapsed begin
+        x_train, y_train = generate_training_data(params)
+    end
+    total_runtime += gen_time
+    timing_info["data_generation_time_s"] = gen_time
+    @info "Data generation time: " gen_time
+    @info "Generating the regressions and region data..."
+    region_time = @elapsed begin
+        region_data = generate_region_images(params, x_train, y_train) 
+    end
+    total_runtime += region_time
+    timing_info["region_bound_time_s"] = region_time 
+    @info "Region generation time: " region_time
+    save_region_data(params, region_data)
+    @info "Generating the transition bounds..."
+    bound_time = @elapsed begin
+        result_mats = generate_transition_bounds(params, region_data)
+    end
+    total_runtime += bound_time
+    timing_info["transition_bound_time_s"] = bound_time 
+    @info "Bound generation time: " bound_time
+    save_transition_matrices(params, result_mats)
+ 
+    safety_result_mat = nothing
+    if single_mode_verification
+        @info "Performing safety verification on single mode..."
+        verification_time = @elapsed begin 
+            # script_path = "/Users/john/Projects/Julia/BMDPVerification"
+            # run_MATLAB_verification(script_path, create_experiment_directory(params), -1, params.verification_mode)
+            safety_result_mat = perform_imdp_verification(params, result_mats, 1)
+        end
+        timing_info["single_verification_time_s"] = verification_time  
+        @info "Verification time: " verification_time
+    end
+
+    @info "Total runtime: " total_runtime
+    timing_info["total_runtime_s"] = total_runtime
+    save_time_info(params, timing_info)
+    
+    flush(logfile)
+    close(logfile)
+
+    return timing_info, safety_result_mat, result_mats
+end
+
 ###############################################################################################
 # Local Methods
 ###############################################################################################
@@ -616,7 +789,7 @@ function create_gp_info(params, gp_set, dim_key)
     gp = gp_set[dim_key]
 
     scale_factor = params.data_params.bound_type == "rkhs-tight" ? params.data_params.noise_sigma/sqrt(1. + 2. / params.data_params.data_num) : 1.
-    @info "Scale factor: $scale_factor"
+    # @info "Scale factor: $scale_factor"
     domain = params.domain
     diam_domain = 0.
     for dim_key in keys(domain)
@@ -627,11 +800,12 @@ function create_gp_info(params, gp_set, dim_key)
 
     # Calculating the RKHS parameter bounds
     RKHS_bound = abs(domain[dim_key][2] + params.system_params.lipschitz_bound*diam_domain)/ σ_inf
-    @info "RKHS Norm Bound in $dim_key: ", RKHS_bound
+    # @info "RKHS Norm Bound in $dim_key: ", RKHS_bound
 
-    B = 1 + (params.data_params.noise_sigma)^(-2)
+    # B = 1 + (params.data_params.noise_sigma)^(-2)
+    B = 1 + (1 + 2/(params.data_params.data_num))^(-1)
     γ = 0.5*params.data_params.data_num*log(B)
-    @info "Info gain term in $dim_key: ", γ
+    # @info "Info gain term in $dim_key: ", γ
 
     K_inv = inv(gp.cK.mat + exp(gp.logNoise.value)^2*I)
     gp_info = GPInfo(gp, γ, RKHS_bound, params.data_params.bound_type, scale_factor, K_inv)
@@ -684,6 +858,18 @@ function process(region_pairs, region_dict, region_post_dict, epsilon, gp_dict, 
 
     for i=1:N
         r[i] = region_pair_transitions(region_pairs[i], region_dict, region_post_dict, epsilon, gp_dict, params)
+    end
+
+    results_df = fetch_results(N, r)
+    return results_df
+end
+
+function process_foo(region_pairs, region_dict, region_post_dict, region_gp_dict, epsilon, params)
+    N = length(region_pairs)
+    r = Array{Array, 1}(undef, N)
+
+    for i=1:N
+        r[i] = region_pair_transitions(region_pairs[i], region_dict, region_post_dict, epsilon, region_gp_dict[region_pairs[i][1]], params)
     end
 
     results_df = fetch_results(N, r)
