2c8a61ada2cd975d9f1aca7619c7dade357f3b6f
John Jackson <jmcjack@gmail.com>
2021-02-23 18:50:16 +0000

    Add direct extent method to IMDP label creation.

--- src/imdp.jl
+++ src/imdp.jl
@@ -41,9 +41,13 @@ function create_simple_imdp(minPr, maxPr)
     return imdp 
 end
 
-function create_imdp_labels(labels_fn, imdp, extent_file)
+function create_imdp_labels(labels_fn, imdp, extent_file::String)
     res = BSON.load(extent_file)
     imdp_state_extents = res[:extents]
+    create_imdp_labels(labels_fn, imdp, imdp_state_extents)
+end
+
+function create_imdp_labels(labels_fn, imdp, imdp_state_extents::Dict)
     for state_key in keys(imdp_state_extents)
         state = imdp_state_extents[state_key]
         if state_key == -11
